f530285aebc7210e755a0f5a7381dd6ffc6be04f
echavez85 <echavez85@gmail.com>
2018-12-10 18:15:40 -0600

    Fixed Spotify keys

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.5b9d1d3ff5d8432b928b804c55d034ae,
-  secret: process.env.b2a7508cff5b40e48d3f63a90256319f
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
