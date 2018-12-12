3b48d2d98a7a263d64a00c37a67b2ca2a7f1bfd1
echavez85 <echavez85@gmail.com>
2018-12-12 11:19:30 -0600

    Added api codes to liri.js.

--- liri.js
+++ liri.js
@@ -1,1 +1,22 @@
 require("dotenv").config();
+
+// require request
+let request = require("request");
+
+// require moment
+const moment = require('moment');
+
+// require file systems
+const fs = require("fs");
+
+// link key page
+const keys = require("./keys.js");
+
+// initialize spotify
+const Spotify = require("node-spotify-api");
+const spotify = new Spotify(keys.spotify);
+
+// OMDB and Bands in Town APIs
+let omdb = (keys.omdb);
+let bandsintown = (keys.bandsintown);
+
