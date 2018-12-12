328bd2989850e590b84c26ce8a22857ae264a9a9
echavez85 <echavez85@gmail.com>
2018-12-12 13:54:21 -0600

    added logic for app.

--- liri.js
+++ liri.js
@@ -10,7 +10,7 @@ const moment = require('moment');
 const fs = require("fs");
 
 // link key page
-const keys = require("./keys.js");
+const keys = require("./keys");
 
 // initialize spotify
 const Spotify = require("node-spotify-api");
@@ -19,4 +19,32 @@ const spotify = new Spotify(keys.spotify);
 // OMDB and Bands in Town APIs
 let omdb = (keys.omdb);
 let bandsintown = (keys.bandsintown);
+
+// take user command and input
+let userInput = process.argv[2];
+let userQuery = process.argv.slice(3).join(" ");
+
+// logic for app
+function userCommand(userInput, userQuery) {
+    // make decision based on the command
+    switch (userInput) {
+        case "concert-this":
+            concertThis();
+            break;
+        case "spotify-this":
+            spotifyThisSong();
+            break;
+        case "movie-this":
+            movieThis();
+            break;
+        case "do-this":
+            doThis(userQuery);
+            break;
+        default:
+            console.log("I don't understand.");
+            break;
+    }
+}
+
+userCommand(userInput, userQuery);
 
