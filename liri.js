35ba970ec54fd32f981dea7eb5be48b2f609b1ca
echavez85 <echavez85@gmail.com>
2018-12-12 14:26:05 -0600

    added concert search code.

--- liri.js
+++ liri.js
@@ -38,7 +38,7 @@ function userCommand(userInput, userQuery) {
             movieThis();
             break;
         case "do-this":
-            doThis(userQuery);
+            doWhatItSays(userQuery);
             break;
         default:
             console.log("I don't understand.");
@@ -48,3 +48,28 @@ function userCommand(userInput, userQuery) {
 
 userCommand(userInput, userQuery);
 
+// concert function
+function concertThis() {
+    console.log(`\n - - - - -\n\nSEARCHING FOR...${userQuery}'s next show...`);
+    // use request as query
+    request("https://rest.bandsintown.com/artists/" + userQuery + "/events?app_id=" + bandsintown, function (error, response, body) {
+        // if everything ok, give ok message
+        if (!error && response.statusCode === 200) {
+            // capture data using JSON to format
+            let userBand = JSON.parse(body);
+            // parse and use for loop to access data
+            if(userBand.length > 0) {
+                for(i=0; i<1; i++) {
+                    // console log data
+                    console.log(`\nWoohoo!!\n\nArtist: ${userBand[i].lineup[0]} \nVenue: ${userBand[i].venue.name}\nVenue Location: ${userBand[i].venue.latitude},${userBand[i].venue.longitude}\nVenue City: ${userBand[i].venue.city}, ${userBand[i].venue.country}`)
+                    // format date with moment
+                    let concertDate = moment(userBand[i].dateTime).format("MM/DD/YYY hh:00 A");
+                    console.log(`Date and Time: ${concertDate}\n\n- - - - -`);
+                };
+                else{
+                    console.log('Band or concert not found!');
+                };
+            };
+        }
+    })
+}
